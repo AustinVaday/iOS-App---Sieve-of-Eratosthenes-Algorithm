185f9386197fc4278dd76311837ef320bfe982c1
Austin Vaday <avaday0@ivc.edu>
2016-02-05 22:15:21 -0800

    Successfully set up the collection view, the collection view cells, and set proper delegates/data sources. Will prettify the collection view/cells next.

--- Sieve-of-Eratosthenes-Algorithm/Sieve-of-Eratosthenes-Algorithm/AlgorithmViewController.swift
+++ Sieve-of-Eratosthenes-Algorithm/Sieve-of-Eratosthenes-Algorithm/AlgorithmViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class AlgorithmViewController: UIViewController {
+class AlgorithmViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
     @IBOutlet weak var numberLabel: UILabel!
 
@@ -17,6 +17,8 @@ class AlgorithmViewController: UIViewController {
     var receivedString: String!
     var receivedNum: Int!
     var sieveObj: SieveOfEratosthenses!
+    
+    let reusableCellIdentifier = "numberCell"
 
     
     override func viewDidLoad() {
@@ -45,4 +47,36 @@ class AlgorithmViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
     
+    
+    // Collection view functionality
+    
+    // Tell the collection view how many cells we need to make
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        // We need a collection of n - 1 cells (the aglorithm does not include last digit of the specified up-to-num)
+        return receivedNum - 1;
+    }
+    
+    // Tell the collection view about the cell we want to use at a particular index of the collection
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        // Reference the storyboard cell
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reusableCellIdentifier, forIndexPath: indexPath) as! NumberCollectionViewCell
+        
+        // Set the cell number
+        cell.cellLabel.text = "1"
+        
+        // Set the background color: 
+        // Green ==> Is a prime number
+        // Red   ==> Is not a prime number
+        cell.backgroundColor = UIColor.blueColor()
+        
+        return cell
+    }
+    
+        
+        
+        
+        
 }
+    
--- Sieve-of-Eratosthenes-Algorithm/Sieve-of-Eratosthenes-Algorithm/NumberCollectionViewCell.swift
+++ Sieve-of-Eratosthenes-Algorithm/Sieve-of-Eratosthenes-Algorithm/NumberCollectionViewCell.swift
@@ -9,5 +9,8 @@
 import UIKit
 
 class NumberCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var cellLabel: UILabel!
+    
 
 }
